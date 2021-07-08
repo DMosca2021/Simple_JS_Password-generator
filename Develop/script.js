1d31e697a9ef50d9a397582ee80a7cf0cfa5fda5
dMosca <devinmosca@yahoo.com>
2021-07-07 22:08:02 -0600

    Working on gen password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,17 @@
 // Assignment Code
+var characters = {
+  alhpa: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  num: "0123456789",
+  symbol: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+};
+
+var userInput = [];
+
+var password = "";
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-
-// series of prompts needed
 
 function writePassword() {
   var password = generatePassword();
@@ -11,6 +19,56 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  var characterLength = 0
+  while ((characterLength < 8 || characterLength > 128) || Number.isInteger(characterLength) === false) {
+      characterLength = parseInt(prompt("How many characters would you like your password to be? (8-128)"));
+  }
+
+  characters.alhpa = false
+  characters.num = false
+  characters.symbol = false
+
+  while (!characters.alhpa && !characters.num && !characters.symbol) {
+    characters.alhpa = confirm("Click OK to confirm upper and lower characters");
+    characters.num = confirm("Click OK to confirm numeric characters");
+    characters.symbol = confirm("Click OK to confirm special characters");
+  }
+
+  if (upper) {
+      userInput.push(charString[0]);
+  }
+
+  if (lower) {
+      userInput.push(charString[1])
+  }
+
+  if (number) {
+      userInput.push(charString[2])
+  }
+
+  if (symbol) {
+      userInput.push(charString[3])
+  }
+
+  var password = "";
+
+
+  userInput = userInput.join("").split("");
+
+  // You should start your for statement at position 0, not 1
+  // and I guess it should be < than characterLength, not userInput.length
+  for (var i = 0; i < characterLength; i++) {
+      // Math.random is a function so you were missing the parenthesis Math.random()
+      // also Math.random() should be times userInput.length
+      // so they should be inside the same parenthesis
+      var index = (Math.floor(Math.random() * userInput.length));
+      password = password + userInput[index]
+  }
+
+  return password
 }
 
 // Add event listener to generate button
